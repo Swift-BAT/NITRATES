c5b986897aaeffe4bdc8c88dfa134e6ad981ef08
Jimmy DeLaunay <delauj2@gmail.com>
2021-11-01 15:38:50 -0500

    Initial upload

---
+++ HeasoftTools/bat_tool_funcs.py
@@ -0,0 +1,97 @@
+import os
+import subprocess
+from gen_tools import run_ftool
+import numpy as np
+
+def ev2dpi(infile, outfile, tstart, tstop, e0, e1, detmask):
+
+    ftool = "batbinevt"
+    arg_list = [infile, outfile, 'DPI', '0', 'uniform', str(e0)+'-'+str(e1)]
+    arg_list += ['tstart='+str(tstart), 'tstop='+str(tstop), 'detmask='+detmask]
+    run_ftool(ftool, arg_list)
+
+def ev2dpi_ebins(infile, outfile, tstart, tstop, ebins, detmask):
+
+    ftool = "batbinevt"
+    arg_list = [infile, outfile, 'DPI', '0', 'uniform', ebins]
+    arg_list += ['tstart='+str(tstart), 'tstop='+str(tstop), 'detmask='+detmask]
+    run_ftool(ftool, arg_list)
+
+
+def bateconvert(infile, outfile, calfile):
+
+    ftool = "bateconvert"
+    arg_list = ['infile='+infile, 'calfile='+calfile, 'residfile=CALDB',
+                'outfile='+outfile, 'clobber=YES', 'pulserfile=CALDB',
+                'fltpulserfile=CALDB']
+    run_ftool(ftool, arg_list)
+
+def detmask(infile, outfile, dmask):
+
+    ftool = "batdetmask"
+    arg_list = [infile, outfile, 'detmask='+dmask]
+    run_ftool(ftool, arg_list)
+
+
+def mk_bkg_mod(infile, outfile, detmask):
+
+    ftool = "batclean"
+    arg_list = [infile, outfile]
+    arg_list += ['incatalog=NONE', 'detmask='+detmask, 'srcclean=NO', 'outversion=bkgfit']
+    run_ftool(ftool, arg_list)
+
+def mk_pc_img(infile, outfile, detmask, attfile, ovrsmp=4, detapp=False):
+
+    ftool = "batfftimage"
+    arg_list = [infile, outfile]
+    arg_list += ['detmask='+detmask, 'attitude='+attfile, 'pcodemap=YES',
+                'clobber=YES']
+    if ovrsmp is not None:
+        arg_list += ['oversampx='+str(ovrsmp),'oversampy='+str(ovrsmp)]
+    if detapp:
+        arg_list.append("aperture=CALDB:DETECTION")
+    run_ftool(ftool, arg_list)
+
+def mk_sky_img(infile, outfile, detmask, attfile, bkg_file=None,\
+                        ovrsmp=2, sig_map=None, bkgvar_map=None,\
+                        detapp=False, rebal=True):
+
+    ftool = "batfftimage"
+    arg_list = [infile, outfile]
+    arg_list += ['detmask='+detmask, 'attitude='+attfile,
+                'oversampx='+str(ovrsmp),'oversampy='+str(ovrsmp),
+                'clobber=YES']
+    if bkg_file is not None:
+        arg_list += ['bkgfile='+bkg_file]
+    if bkgvar_map is not None:
+        arg_list += ['bkgvarmap='+bkgvar_map]
+    if sig_map is not None:
+        arg_list += ['signifmap='+sig_map]
+    if detapp:
+        arg_list.append("aperture=CALDB:DETECTION")
+    if rebal:
+        arg_list.append("rebalance=YES")
+    else:
+        arg_list.append("rebalance=NO")
+    run_ftool(ftool, arg_list)
+
+
+def run_batcelldetect(infile, cat_fname, snr_thresh=3.5,\
+                    sigmap=None, bkgvar=None, ovrsmp=2,\
+                    incat="NONE", pcode="NONE"):
+
+    ftool = "batcelldetect"
+    bkgradius = 15*ovrsmp
+    srcradius = 6*ovrsmp
+    arg_list = ["infile="+infile, "outfile="+cat_fname, "snrthresh="+str(snr_thresh)]
+    arg_list += ["bkgradius="+str(bkgradius),
+                "srcradius="+str(srcradius),
+                "nadjpix=2", "vectorflux=YES",
+                "incatalog="+incat, "niter=4",
+                "pcodefile="+pcode, "chatter=1"]
+    if sigmap is not None:
+        arg_list.append('signifmap='+sigmap)
+    if bkgvar is not None:
+        arg_list.append('bkgvarmap='+bkgvar)
+    print arg_list
+    run_ftool(ftool, arg_list)
---
+++ do_sub_sim_jobs.py
@@ -0,0 +1,92 @@
+import time
+import numpy as np
+# import pandas as pd
+# import healpy as hp
+# from astropy.table import Table
+# from astropy.io import fits
+# from astropy.wcs import WCS
+# from scipy import interpolate
+# import os, socket, subprocess, shlex
+import os
+import argparse
+import logging, traceback
+# import paramiko
+
+# from helper_funcs import send_email, send_error_email, send_email_attach, send_email_wHTML
+
+from do_manage import sub_jobs
+
+
+def cli():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--Nratejobs', type=int,\
+            help="Total number of jobs",
+            default=64)
+    parser.add_argument('--Nllhjobs', type=int,\
+            help="Total number of jobs",
+            default=128)
+    parser.add_argument('--Name', type=str,\
+            help="Name of the event to submit jobs as",\
+            default='')
+    parser.add_argument('--queue', type=str,\
+            help="Name of the queue to submit jobs to",\
+            default='cyberlamp')
+    parser.add_argument('--qos', type=str,\
+            help="qos option",\
+            default=None)
+    parser.add_argument('--BKGpyscript', type=str,\
+            help="Name of python script for Bkg Estimation",\
+            default='do_bkg_estimation_wPSs.py')
+    parser.add_argument('--RATEpyscript', type=str,\
+            help="Name of python script for Rates analysis",\
+            default='do_rates_mle_wPSs_4sims.py')
+    parser.add_argument('--LLHpyscript', type=str,\
+            help="Name of python script for LLH analysis",\
+            default='do_llh_forSims.py')
+    parser.add_argument('--SCANpyscript', type=str,\
+            help="Name of python script for FoV scan",\
+            default='do_llh_scan_wPSs_realtime.py')
+    parser.add_argument('--do_bkg',\
+            help="Submit the BKG estimation script",\
+            action='store_true')
+    parser.add_argument('--do_rates',\
+            help="Submit the Rate jobs",\
+            action='store_true')
+    parser.add_argument('--do_llh',\
+            help="Submit the llh jobs",\
+            action='store_true')
+    parser.add_argument('--do_scan',\
+            help="Submit the llh scan jobs",\
+            action='store_true')
+    parser.add_argument('--pbs_fname', type=str,\
+            help="Name of pbs script",\
+            default='/storage/work/jjd330/local/bat_data/BatML/submission_scripts/pyscript_template.pbs')
+    parser.add_argument('--sim_dir', type=str,\
+            help="Name of simulation directory",\
+            default=None)
+    args = parser.parse_args()
+    return args
+
+
+def main(args):
+
+    if args.do_rates:
+        extra_args = "--sim_dir " + args.sim_dir
+        sub_jobs(args.Nratejobs, 'RATES_'+args.Name, args.RATEpyscript,\
+                    args.pbs_fname, queue=args.queue, qos=args.qos,\
+                    extra_args=extra_args)
+    if args.do_llh:
+        extra_args = "--sim_dir " + args.sim_dir
+        sub_jobs(args.Nllhjobs, 'LLH_'+args.Name, args.LLHpyscript,\
+                    args.pbs_fname, queue=args.queue, qos=args.qos,\
+                    extra_args=extra_args)
+    if args.do_scan:
+        sub_jobs(args.Nllhjobs, 'SCAN_'+args.Name, args.SCANpyscript,\
+                    args.pbs_fname, queue=args.queue, qos=args.qos)
+
+
+if __name__ == "__main__":
+
+    args = cli()
+
+    main(args)
