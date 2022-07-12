6d11cde485163b4f10f70e17667f3133bbf3fa32
Tyler Parsotan <parsotat@umbc.edu>
2022-07-11 20:35:34 -0400

    Modify do_rates_mle_InOutFoV2 to use the config variables which will be able to be set by the user in various function calls.

---
+++ nitrates/__init__.py
@@ -0,0 +1,15 @@
+__version__ = '0.1a1'  # make sure this matches the setup.py
+
+import config
+from .analysis_seeds import *
+from .archive import *
+from .data_prep import *
+from .data_scraping import *
+from .HeasoftTools import *
+from .imaging import *
+from .lib import *
+from .listeners import *
+from .llh_analysis import *
+from .models import *
+from .post_process import *
+from .response import *
--- nitrates/config.py
+++ nitrates/config.py
@@ -1,4 +1,9 @@
+"""
+This fine allows the user to configure where the data is located on their system and keeps the changes persistent through the whole code.
+"""
+
 import os
+
 
 NITRATES_RESP_DIR="/Users/tparsota/Documents/BAT_SCRIPTS/NITRATES_BAT_RSP_FILES/" # env variable can be used or this can be set
 if NITRATES_RESP_DIR is None:
@@ -7,8 +12,8 @@ if NITRATES_RESP_DIR is None:
     # if NITRATES_RESP_DIR is not set here or as an env var then
     # it's assumed to be in the current working direc
     NITRATES_RESP_DIR='.'
-
-# ray traces directory
+    
+    # ray traces directory
 rt_dir = os.path.join(NITRATES_RESP_DIR,'ray_traces_detapp_npy')
 # Resp Table with direct response directory
 RESP_TAB_DNAME = os.path.join(NITRATES_RESP_DIR,'resp_tabs_ebins')
@@ -16,13 +21,18 @@ RESP_TAB_DNAME = os.path.join(NITRATES_RESP_DIR,'resp_tabs_ebins')
 COMP_FLOR_RESP_DNAME = os.path.join(NITRATES_RESP_DIR,'comp_flor_resps')
 # Directory with Flor response only
 HP_FLOR_RESP_DNAME = os.path.join(NITRATES_RESP_DIR,'hp_flor_resps')
-# Directory with the element cross section data files
-ELEMENT_CROSS_SECTION_DNAME = os.path.join(NITRATES_RESP_DIR,'element_cross_sections')
-
 # DPI with st per det exposed to sky
 solid_angle_dpi_fname = os.path.join(NITRATES_RESP_DIR,'solid_angle_dpi.npy')
+
+
+#get the directory that the data directory is located in
+dir = os.path.split(__file__)[0]
+
+# Directory with the element cross section data files
+ELEMENT_CROSS_SECTION_DNAME = os.path.join(dir, "data",'element_cross_sections')
+
 # Table of bright known sources from the Trans Monitor
-bright_source_table_fname = os.path.join(NITRATES_RESP_DIR,'bright_src_cat.fits')
+bright_source_table_fname = os.path.join(dir, "data",'bright_src_cat.fits')
 
 EBINS0 = [15., 24., 35., 48., 64., 84., 120., 171.5, 245.]
 EBINS1 = [24., 35., 48., 64., 84., 120., 171.5, 245., 350.]
@@ -32,8 +42,8 @@ EBINS1 = [24., 35., 48., 64., 84., 120., 171.5, 245., 350.]
 ###   Old Stuff ###
 ###########################
 
-bat_ml_dir='/storage/work/jjd330/local/bat_data/BatML/'
-ftool_wrap='/storage/work/jjd330/local/bat_data/BatML/HeasoftTools/run_ftool.sh'
+#bat_ml_dir='/storage/work/jjd330/local/bat_data/BatML/'
+#ftool_wrap='/storage/work/jjd330/local/bat_data/BatML/HeasoftTools/run_ftool.sh'
 
 # rt_dir='/gpfs/scratch/jjd330/bat_data/ray_traces_detapp_npy/'
 # fp_dir = '/gpfs/scratch/jjd330/bat_data/footprints_npy/'
@@ -43,14 +53,14 @@ ftool_wrap='/storage/work/jjd330/local/bat_data/BatML/HeasoftTools/run_ftool.sh'
 # solid_angle_dpi_fname='/storage/work/jjd330/local/bat_data/solid_angle_dpi.npy'
 # bright_source_table_fname='/storage/work/jjd330/local/bat_data/BatML/bright_src_cat.fits'
 
-HEADAS="/storage/work/jjd330/heasoft/heasoft-6.21/x86_64-unknown-linux-gnu-libc2.12"
-HEADAS_INIT="/storage/work/jjd330/heasoft/heasoft-6.21/x86_64-unknown-linux-gnu-libc2.12/headas-init.sh"
-CALDB="/storage/work/jjd330/caldb_files"
-CALDB_INIT="/storage/work/jjd330/caldb_files/software/tools/caldbinit.sh"
+#HEADAS="/storage/work/jjd330/heasoft/heasoft-6.21/x86_64-unknown-linux-gnu-libc2.12"
+#HEADAS_INIT="/storage/work/jjd330/heasoft/heasoft-6.21/x86_64-unknown-linux-gnu-libc2.12/headas-init.sh"
+#CALDB="/storage/work/jjd330/caldb_files"
+#CALDB_INIT="/storage/work/jjd330/caldb_files/software/tools/caldbinit.sh"
 
 # EBINS0 = [14., 24., 36.3, 55.4, 80.0, 120.7]
 # EBINS1 = [24., 36.3, 55.4, 80.0, 120.7, 194.9]
-
+"""
 quad_dicts = {'all':{'quads':[0,1,2,3],
                      'drm_fname':'drm_0.200_0.150_.fits',
                     'imx':0.2, 'imy':0.15, 'id':0},
@@ -79,3 +89,4 @@ quad_dicts = {'all':{'quads':[0,1,2,3],
                     'drm_fname':'drm_-1.100_0.500_.fits',
                     'imx':-1.1, 'imy':0.5, 'id':8}
                 }
+"""
