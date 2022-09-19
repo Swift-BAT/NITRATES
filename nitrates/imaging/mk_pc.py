c8a9ce98d0721985d42719369cc97de0a6dcc430
Tyler Parsotan <parsotat@umbc.edu>
2022-09-19 15:57:19 -0400

    Fixed importing mk_pc.py.

--- nitrates/imaging/mk_pc.py
+++ nitrates/imaging/mk_pc.py
@@ -1,3 +1,4 @@
 from ..lib.funcs2run_bat_tools import do_pc
 
-do_pc('detmask.fits', 'attitude.fits', '.', ovrsmp=2, detapp=True)
+if __name__ == "__main__":
+    do_pc('detmask.fits', 'attitude.fits', '.', ovrsmp=2, detapp=True)
