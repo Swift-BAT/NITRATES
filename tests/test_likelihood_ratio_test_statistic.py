30a3a976e7295c3c853d25da49c23d4d62b7d366
Jimmy DeLaunay <delauj2@gmail.com>
2024-08-12 09:57:29 -0500

    up rel_tol to 1e-4

--- tests/test_likelihood_ratio_test_statistic.py
+++ tests/test_likelihood_ratio_test_statistic.py
@@ -333,7 +333,7 @@ def calculate_LLH_ratio_test_statistic(theta, phi):
 # signal at theta, phi = 38.541, 137.652, (or given as ra, dec = 233.117, -26.213)
 def test_LLH_ratio_test_statistic_IFOV():
     sqrtTS = calculate_LLH_ratio_test_statistic(38.541, 137.652)
-    assert math.isclose(sqrtTS, 17.0090525819679, rel_tol=1e-05) == True
+    assert math.isclose(sqrtTS, 17.0090525819679, rel_tol=1e-04) == True
 
 
 #################################################################################
@@ -341,7 +341,7 @@ def test_LLH_ratio_test_statistic_IFOV():
 # signal at theta, phi = 125.0, 25.0.
 def test_LLH_ratio_test_statistic_OFOV():
     sqrtTS = calculate_LLH_ratio_test_statistic(125.0, 25.0)
-    assert math.isclose(sqrtTS, 15.516484046540924, rel_tol=1e-05) == True
+    assert math.isclose(sqrtTS, 15.516484046540924, rel_tol=1e-04) == True
 
 
 #################################################################################
@@ -350,13 +350,13 @@ def test_LLH_ratio_test_statistic_OFOV():
 def old_test_LLH_ratio_test_statistic_IFOV():
     sqrtTS = old_calculate_LLH_ratio_test_statistic(38.541, 137.652)
     print("Old sqrtTS IFOV: ", sqrtTS)
-    assert math.isclose(sqrtTS, 17.0090525819679, rel_tol=1e-05) == True
+    assert math.isclose(sqrtTS, 17.0090525819679, rel_tol=1e-04) == True
 
 
 def old_test_LLH_ratio_test_statistic_OFOV():
     sqrtTS = old_calculate_LLH_ratio_test_statistic(125.0, 25.0)
     print("Old sqrtTS OFOV: ", sqrtTS)
-    assert math.isclose(sqrtTS, 15.516484046540924, rel_tol=1e-05) == True
+    assert math.isclose(sqrtTS, 15.516484046540924, rel_tol=1e-04) == True
 
 
 if __name__ == "__main__":
